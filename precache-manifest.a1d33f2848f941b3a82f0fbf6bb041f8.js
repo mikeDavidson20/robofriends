d5ee37bbe12fe3aead2b6de676b5ac41d8a03290
Mike Davidson <michaelschnurman@gmail.com>
2022-05-19 18:14:06 -0400

    Updates

---
+++ precache-manifest.a1d33f2848f941b3a82f0fbf6bb041f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "74d05d9a4a61a09dcf7a",
+    "url": "https//mikeDavidson20.github.io/robofriends/static/css/main.1f5f1c93.chunk.css"
+  },
+  {
+    "revision": "74d05d9a4a61a09dcf7a",
+    "url": "https//mikeDavidson20.github.io/robofriends/static/js/main.74d05d9a.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "https//mikeDavidson20.github.io/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "d9688fb3fbd276420bee",
+    "url": "https//mikeDavidson20.github.io/robofriends/static/css/2.fe01ad06.chunk.css"
+  },
+  {
+    "revision": "d9688fb3fbd276420bee",
+    "url": "https//mikeDavidson20.github.io/robofriends/static/js/2.d9688fb3.chunk.js"
+  },
+  {
+    "revision": "c3da39c518ae41011c51",
+    "url": "https//mikeDavidson20.github.io/robofriends/static/js/runtime~main.c3da39c5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "https//mikeDavidson20.github.io/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a281a0345d801a78b9580d7e5f6ad5b4",
+    "url": "https//mikeDavidson20.github.io/robofriends/index.html"
+  }
+];
